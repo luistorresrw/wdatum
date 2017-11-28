24c85a17ad24e760aa6473b9b8418bd1fc715b86
jaime <jaimeceballos_rw@hotmail.com>
2017-11-28 10:14:26 -0300

    endpoints establecimiento y familia

--- wdatum/urls.py
+++ wdatum/urls.py
@@ -7,7 +7,6 @@ from rest_framework.authtoken import views as token_views
 router = routers.DefaultRouter()
 router.register(r'users',views.UserViewSet)
 router.register(r'groups',views.GroupviewSet)
-router.register(r'regimen_tenencia', views.RegimenTenenciaViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
